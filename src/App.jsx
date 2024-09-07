f598042cf2c150e9fcc7a6174ef5047579fa03aa
parkjuyeong <dcmo77777@gmail.com>
2024-09-07 11:29:40 +0900

    Router기능추가 및 기본설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainPage from './pages/main'
+import LoginPage from './pages/login'
+import RankPage from './pages/rank'
 
 function App() {
-
   return (
-    <MainPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/rank" element={<RankPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/rank.jsx
@@ -0,0 +1,5 @@
+const RankPage = () => {
+  return <div>rank</div>
+}
+
+export default RankPage
