bf76003a132292215a7829c752afdbacc65faa51
ktollonr <agetor3210@naver.com>
2024-09-09 22:26:43 +0900

    [INIT] RankPage 브랜치 생성

--- src/pages/rank.jsx
+++ src/pages/rank.jsx
@@ -1,5 +1,5 @@
 //import styled from 'styled-components'
-//import { useState } from 'react'
+import { useState } from 'react'
 import RankMent from '../layouts/RankMent'
 import TierImage from '../layouts/TierImage';
 import Ranking from '../layouts/Ranking';
