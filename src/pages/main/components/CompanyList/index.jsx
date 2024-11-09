9fb564ff56f216a08124b808dd5b9b3cd916128e
dcmo77 <dcmo77@naver.com>
2024-11-10 04:43:00 +0900

    [FEAT] CompanyList sideBar company별로 렌더링, 등락률 적용

--- src/pages/main/components/CompanyList/index.jsx
+++ src/pages/main/components/CompanyList/index.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import UpDownText from '../../../../components/UpDownText';
 import { useCompanyData } from '../../../../hooks/useCompanyData';
+import { useEffect, useState } from 'react';
+import { API } from '../../../../api/axios';
+import { useChartData } from '../../../../hooks/useChart';
 
 const ButtonBox = styled.button`
   display: flex;
@@ -28,21 +31,37 @@ const LogoImage = styled.img`
   margin-right: 8px;
 `;
 
-function CompanyList({ name, logo, currentPrice, buyPrice, onClick }) {
+function CompanyList({ name, logo, companyId, initialStockPrice, investmentAmount, onClick }) {
   const { companyList, activeCompany } = useCompanyData();
+  const [sidecompany, setSideCompany] = useState(null);
+  const { chartData } = useChartData();
+  useEffect(() => {
+    const fetchChartData = async () => {
+      try {
+        const response = await API.get(`/company/${companyId}/charts`);
+        setSideCompany(response.data);
+      } catch (error) {
+        console.error('Error fetching chart data:', error);
+      }
+    };
+
+    fetchChartData();
+  }, [chartData]);
 
   return (
     <>
-      {companyList && activeCompany && (
+      {companyList && activeCompany && sidecompany && (
         <ButtonBox onClick={onClick}>
           <div style={{ display: 'flex', alignItems: 'center' }}>
             <LogoImage src={logo} alt={`${name} 로고`} />
             <div style={{ fontSize: 20 }}>{name}</div>
           </div>
           <div style={{ textAlign: 'right' }}>
-            <div>{currentPrice.toLocaleString()}원</div>
-            <UpDownText standard={buyPrice} comparision={currentPrice} />
+            <div>{investmentAmount.toLocaleString()}원</div>
+            {/* {console.log(sidecompany)} */}
+            <UpDownText standard={initialStockPrice * 100} comparision={sidecompany.chartList[0].high} />
           </div>
+          {/* initialStockPrice=38866  sidecompany.chartList[0].high=4034880 */}
         </ButtonBox>
       )}
     </>
--- src/pages/rank/components/Ranking/index.jsx
+++ src/pages/rank/components/Ranking/index.jsx
@@ -1,17 +1,17 @@
-import styled from 'styled-components'
-import UpDownText from '../../../../components/UpDownText'
+import styled from 'styled-components';
+import UpDownText from '../../../../components/UpDownText';
 
-import { useRanking } from '../../../../hooks/useRanking'
-import { useUser } from '../../../../hooks/useUser'
+import { useRanking } from '../../../../hooks/useRanking';
+import { useUser } from '../../../../hooks/useUser';
 
-import LoadingSpinner from '../../../../styles/LoadingSpinner'
+import LoadingSpinner from '../../../../styles/LoadingSpinner';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
 
-  border-radius:  ${(props) => props.theme.border.radius.small};
-`
+  border-radius: ${props => props.theme.border.radius.small};
+`;
 
 const UserContainer = styled.div`
   margin-top: 26px;
@@ -23,10 +23,10 @@ const UserContainer = styled.div`
   align-items: center;
   justify-content: space-between;
 
-  background-color:  ${(props) => props.theme.colors.grey.border};
-  border-radius: ${(props) => props.theme.border.radius.small};
+  background-color: ${props => props.theme.colors.grey.border};
+  border-radius: ${props => props.theme.border.radius.small};
   box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.2);
-`
+`;
 
 const Circle = styled.div`
   display: flex;
@@ -37,49 +37,49 @@ const Circle = styled.div`
   height: 70px;
   width: 70px;
 
-  background-color: ${(props) => props.theme.colors.blue.primary};
-`
+  background-color: ${props => props.theme.colors.blue.primary};
+`;
 
 const CircleFont = styled.div`
   color: #ffffff;
   font-size: 48px;
-  font-weight: ${(props) => props.theme.font.weight.bold};
+  font-weight: ${props => props.theme.font.weight.bold};
 
   background: none;
-  border: none;  
-`
+  border: none;
+`;
 
 const InfoFont = styled.div`
   margin-left: 30px;
 
   font-size: 48px;
-  font-weight: ${(props) => props.theme.font.weight.bold};
-`
+  font-weight: ${props => props.theme.font.weight.bold};
+`;
 
 const TimeFont = styled.div`
-  color: ${(props) => props.theme.colors.grey.light};
+  color: ${props => props.theme.colors.grey.light};
   opacity: 0.3;
-  
-  font-size: 32px;
-  font-weight: ${(props) => props.theme.font.weight.bold};
-  
-  margin-left: auto;
-`
 
-let date = new Date()
-const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
+  font-size: 32px;
+  font-weight: ${props => props.theme.font.weight.bold};
+
+  margin-left: auto;
+`;
+
+let date = new Date();
+const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
 
 function Ranking({ data }) {
   const { ranking, setRanking, userRanking, setUserRanking } = useRanking();
   const { userData, totalAssets } = useUser();
 
   if (!Array.isArray(ranking) || !userData) {
-    return <LoadingSpinner/>
+    return <LoadingSpinner />;
   }
 
   return (
     <Container>
-      <TimeFont style={{marginTop: "20px"}}>{formattedDate} 00:00 기준</TimeFont>
+      <TimeFont style={{ marginTop: '20px' }}>{formattedDate} 00:00 기준</TimeFont>
       <Container
         style={{
           padding: '10px',
@@ -88,19 +88,14 @@ function Ranking({ data }) {
           maxWidth: '900px',
           overflowY: 'auto',
         }}
-        >
+      >
         {ranking.map((item, index) => (
-          <UserContainer
-            key={index}
-            style={{ marginTop: index === 0 ? '0px' : '26px', minWidth: '880px',}}
-          >
+          <UserContainer key={index} style={{ marginTop: index === 0 ? '0px' : '26px', minWidth: '880px' }}>
             <Circle>
               <CircleFont>{index + 1}</CircleFont>
             </Circle>
             <InfoFont>{item.userRealName}</InfoFont>
-            <InfoFont style={{ marginRight: '30px' }}>
-              {item.totalAssets.toLocaleString()}원
-            </InfoFont>
+            <InfoFont style={{ marginRight: '30px' }}>{item.totalAssets.toLocaleString()}원</InfoFont>
             {/* <UpDownText
               standard={chartData[chartData.length - 1][2]}
               comparision={chartData[chartData.length - 1][3]}
@@ -117,7 +112,7 @@ function Ranking({ data }) {
         <InfoFont>{totalAssets.toLocaleString()}원</InfoFont>
       </UserContainer>
     </Container>
-  )
+  );
 }
 
-export default Ranking
+export default Ranking;
