bbac706472547fac38fc31362be58e34fd885274
dcmo77 <dcmo77@naver.com>
2024-11-09 18:04:02 +0900

    [FEAT] TodoProvider생성 및 Todo추가시 rendering 깜빡거림 증상 해결완료

--- src/hooks/useTodo.jsx
+++ src/hooks/useTodo.jsx
@@ -1,22 +1,53 @@
-import { useEffect, useState } from 'react';
+// TodoContext.jsx
+import { createContext, useContext, useEffect, useState } from 'react';
 import { API } from '../api/axios';
+import { useCompanyData } from './useCompanyData';
+import { useUser } from './useUser';
+import { format } from 'date-fns';
+import { useDate } from '../pages/main/hooks/useDate';
 
-export const useTodo = () => {
-  //   const [userData, setUserData] = useState(null);
-  //   //유저 정보 불러오기
-  //   useEffect(() => {
-  //     const fetchUserData = async () => {
-  //       try {
-  //         const result = await API.get('/users/1');
-  //         setUserData(result.data);
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     };
-  //     fetchUserData();
-  //   }, []);
-  //   return {
-  //     userData,
-  //     setUserData,
-  //   };
+// Context 생성
+const TodoContext = createContext();
+
+// Provider 컴포넌트 구현
+export const TodoProvider = ({ children }) => {
+  const [todoList, setTodoList] = useState(null);
+  const { selectedDate, setSelectedDate } = useDate();
+  const { userData } = useUser();
+  const { activeCompany } = useCompanyData();
+
+  // 할 일 목록을 가져오는 함수
+  const fetchTodoList = async () => {
+    try {
+      const formattedDate = format(selectedDate, 'yyyy-MM-dd'); // 날짜 포맷을 'yyyy-MM-dd'로 변환
+      const result = await API.get(`/todo?companyId=${activeCompany.id}&date=${formattedDate}`);
+      setTodoList(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 의존성 배열을 이용해 userData, activeCompany, selectedDate가 변경될 때 fetchTodoList 호출
+  useEffect(() => {
+    if (userData && activeCompany && selectedDate) {
+      fetchTodoList();
+    }
+  }, [userData, activeCompany, selectedDate]);
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todoList,
+        setTodoList,
+        selectedDate,
+        setSelectedDate,
+        fetchTodoList,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
 };
+
+// Context를 쉽게 사용할 수 있도록 커스텀 훅 생성
+export const useTodo = () => useContext(TodoContext);
