861ac0a8e5a6f7725ecf9a0a8789849b73c818b8
dcmo77 <dcmo77@naver.com>
2024-11-10 22:37:35 +0900

    [FEAT] temp

--- src/pages/main/components/TotalSum/index.jsx
+++ src/pages/main/components/TotalSum/index.jsx
@@ -5,22 +5,18 @@ import { useCompanyData } from '../../../../hooks/useCompanyData';
 import { useUser } from '../../../../hooks/useUser';
 
 const GrayText = styled.div`
-  font-size: ${(props) => props.theme.font.size.primary};
-  font-weight: ${(props) => props.theme.font.weight.extraBold};
-  color: ${(props) => props.theme.colors.grey.light};
+  font-size: ${props => props.theme.font.size.primary};
+  font-weight: ${props => props.theme.font.weight.extraBold};
+  color: ${props => props.theme.colors.grey.light};
 `;
 
 function TotalSum() {
   const { companyList } = useCompanyData();
   const { userData, setUserData, totalAssets, setTotalAssets } = useUser();
 
-  const currentValue = SumList({ data: companyList, type: 'currentStockPrice'}); // 현재가
-  
-  return (
-    <GrayText>
-      총 {currentValue.toLocaleString()}원
-    </GrayText>
-  );
+  const currentValue = SumList({ data: companyList, type: 'currentStockPrice' }); // 현재가
+
+  return <GrayText>총 {currentValue.toLocaleString()}원</GrayText>;
 }
 
 export default TotalSum;
