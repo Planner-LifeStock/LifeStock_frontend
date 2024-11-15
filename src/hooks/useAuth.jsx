3738c6ca9019bdfd61612c1e6b59c21b67509d9f
dcmo77 <dcmo77@naver.com>
2024-11-15 10:33:11 +0900

    [배포] useAuth RefreshToken url 수정

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -53,7 +53,7 @@ export const AuthProvider = ({ children }) => {
       if (accessToken && checkTokenExp(accessToken)) {
         try {
           const response = await axios.post(
-            'http://3.36.201.192:8080/auth/refresh',
+            'https://lifestock.work:8080/auth/refresh',
             {},
             {
               headers: {
