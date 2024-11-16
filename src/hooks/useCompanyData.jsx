6e6fcaa9f7fe1ba64fd2ac1a4d991de59e728589
dcmo77 <dcmo77@naver.com>
2024-11-16 23:18:51 +0900

    [fix] totalEvaluationAmount

--- src/hooks/useCompanyData.jsx
+++ src/hooks/useCompanyData.jsx
@@ -14,7 +14,6 @@ export const CompanyProvider = ({ children }) => {
   console.log(totalAssets);
 
   const seedMoney = 10000000; // 시드머니
-  const totalEvaluationAmount = seedMoney + realizedProfitLoss; // 총평가 -> 문제없음
   const totalPurchaseAmount = SumList({ data: companyList, type: 'investmentAmount' }); // 총매입
   const realizedProfitLoss =
     Array.isArray(soldCompany) && soldCompany.length > 0
@@ -22,6 +21,7 @@ export const CompanyProvider = ({ children }) => {
       : 0; // 실현손익 잘됨
   const unrealizedProfitLoss = totalAssets - seedMoney; // 평가손익-> totalAssets 문제 로직 교체
   const totalProfitLoss = realizedProfitLoss + unrealizedProfitLoss; // 총손익
+  const totalEvaluationAmount = seedMoney + realizedProfitLoss; // 총평가 -> 문제없음
   const totalReturnRate = (unrealizedProfitLoss / seedMoney) * 100; // 수익률
 
   const fetchCompanyData = async () => {
