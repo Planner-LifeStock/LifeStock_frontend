d0e27b024e6a10fa7db80eae7ed3879726f4ddc7
dcmo77 <dcmo77@naver.com>
2024-11-16 23:13:42 +0900

    [fix] totalAsset로직 오류 찾기

--- src/hooks/useCompanyData.jsx
+++ src/hooks/useCompanyData.jsx
@@ -11,17 +11,17 @@ export const CompanyProvider = ({ children }) => {
   const [soldCompany, setSoldCompany] = useState(null);
   const [loading, setLoading] = useState(true);
   const { totalAssets } = useUser();
-  
+  console.log(totalAssets);
+
   const seedMoney = 10000000; // 시드머니
+  const totalEvaluationAmount = seedMoney + realizedProfitLoss; // 총평가 -> 문제없음
   const totalPurchaseAmount = SumList({ data: companyList, type: 'investmentAmount' }); // 총매입
   const realizedProfitLoss =
-  Array.isArray(soldCompany) && soldCompany.length > 0
-    ? SumList({ data: soldCompany, type: 'listedStockPrice' }) * 100 -
-    SumList({ data: soldCompany, type: 'investmentAmount' })
-    : 0;
-  const unrealizedProfitLoss = totalAssets - seedMoney; // 평가손익
+    Array.isArray(soldCompany) && soldCompany.length > 0
+      ? SumList({ data: soldCompany, type: 'listedStockPrice' }) * 100 - SumList({ data: soldCompany, type: 'investmentAmount' })
+      : 0; // 실현손익 잘됨
+  const unrealizedProfitLoss = totalAssets - seedMoney; // 평가손익-> totalAssets 문제 로직 교체
   const totalProfitLoss = realizedProfitLoss + unrealizedProfitLoss; // 총손익
-  const totalEvaluationAmount = seedMoney + realizedProfitLoss; // 총평가
   const totalReturnRate = (unrealizedProfitLoss / seedMoney) * 100; // 수익률
 
   const fetchCompanyData = async () => {
@@ -61,7 +61,7 @@ export const CompanyProvider = ({ children }) => {
         fetchCompanyData,
         totalPurchaseAmount,
         realizedProfitLoss,
-        unrealizedProfitLoss, 
+        unrealizedProfitLoss,
         totalProfitLoss,
         totalEvaluationAmount,
         totalReturnRate,
