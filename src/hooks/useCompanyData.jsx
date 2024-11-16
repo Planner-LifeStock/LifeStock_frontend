3acea7c6db21b3e4af76c5a2170002cd3f9c5ba3
dcmo77 <dcmo77@naver.com>
2024-11-16 23:39:25 +0900

    [fix] 평가손익 로직 교체

--- src/hooks/useCompanyData.jsx
+++ src/hooks/useCompanyData.jsx
@@ -11,15 +11,15 @@ export const CompanyProvider = ({ children }) => {
   const [soldCompany, setSoldCompany] = useState(null);
   const [loading, setLoading] = useState(true);
   const { totalAssets } = useUser();
-  console.log(totalAssets);
 
   const seedMoney = 10000000; // 시드머니
   const totalPurchaseAmount = SumList({ data: companyList, type: 'investmentAmount' }); // 총매입
+  const currentStockSum = SumList({ data: companyList, type: currentStockPrice });
   const realizedProfitLoss =
     Array.isArray(soldCompany) && soldCompany.length > 0
       ? SumList({ data: soldCompany, type: 'listedStockPrice' }) * 100 - SumList({ data: soldCompany, type: 'investmentAmount' })
       : 0; // 실현손익 잘됨
-  const unrealizedProfitLoss = totalAssets - seedMoney; // 평가손익-> totalAssets 문제 로직 교체
+  const unrealizedProfitLoss = currentStockSum - totalPurchaseAmount; // 평가손익-> totalAssets 문제 로직 교체
   const totalProfitLoss = realizedProfitLoss + unrealizedProfitLoss; // 총손익
   const totalEvaluationAmount = seedMoney + realizedProfitLoss; // 총평가 -> 문제없음
   const totalReturnRate = (unrealizedProfitLoss / seedMoney) * 100; // 수익률
