ecb1cd49057d824cfc3581bdc49d3b8440d3142d
ktollonr <agetor3210@naver.com>
2024-11-18 02:22:31 +0900

    모바일 상태 관리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 import Header from './layouts/Header';
 import MainPage from './pages/main';
@@ -31,34 +32,45 @@ const Layout = () => {
 
 function App() {
   return (
-    <AuthProvider>
-      <UserProvider>
-        <CompanyProvider>
-          <ChartProvider>
-            <BrowserRouter>
-              <Layout />
-              <Routes>
-                {/* 로그인 및 회원가입 페이지는 RequireAuth 없이 접근 가능 */}
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
+    <>
+      <MobileView>
+        <div style={{display: 'flex',
+                     fontWeight: 'bold',
+                     justifyContent: 'center',
+                     alignItems: 'center',
+        }}>모바일은 추후 지원 예정입니다!</div>
+      </MobileView>
+      <BrowserView>
+        <AuthProvider>
+          <UserProvider>
+            <CompanyProvider>
+              <ChartProvider>
+                <BrowserRouter>
+                  <Layout />
+                  <Routes>
+                    {/* 로그인 및 회원가입 페이지는 RequireAuth 없이 접근 가능 */}
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
 
-                {/* RequireAuth를 적용하여 메인 페이지 보호 */}
-                <Route element={<RequireAuth />}>
-                  <Route path="/" element={<MainPage />} />
-                  <Route path="/myasset" element={<MyAssetPage />} />
-                  <Route path="/salesrecords" element={<SalesHistoryPage />} />
-                  <Route path="/rank" element={<RankPage />} />
-                  <Route path="/tutorial" element={<Tutorial/>} />
-                </Route>
+                    {/* RequireAuth를 적용하여 메인 페이지 보호 */}
+                    <Route element={<RequireAuth />}>
+                      <Route path="/" element={<MainPage />} />
+                      <Route path="/myasset" element={<MyAssetPage />} />
+                      <Route path="/salesrecords" element={<SalesHistoryPage />} />
+                      <Route path="/rank" element={<RankPage />} />
+                      <Route path="/tutorial" element={<Tutorial/>} />
+                    </Route>
 
-                {/* 일치하지 않는 경로를 메인 페이지로 리디렉션 */}
-                <Route path="*" element={<Navigate to="/" />} />
-              </Routes>
-            </BrowserRouter>
-          </ChartProvider>
-        </CompanyProvider>
-      </UserProvider>
-    </AuthProvider>
+                    {/* 일치하지 않는 경로를 메인 페이지로 리디렉션 */}
+                    <Route path="*" element={<Navigate to="/" />} />
+                  </Routes>
+                </BrowserRouter>
+              </ChartProvider>
+            </CompanyProvider>
+          </UserProvider>
+        </AuthProvider>
+      </BrowserView>
+    </>
   );
 }
 
